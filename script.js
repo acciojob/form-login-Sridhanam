fda8061702664f9922ccf86dea754079b6c46069
Sridhanam <dharsri255@gmail.com>
2025-06-30 08:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 function getFormvalue() {
+    //Write your code here
 	const f = document.getElementById('name1').value;
 	const l = document.getElementById('name2').value;
 	alert(`${f} ${l}`);
-    //Write your code here
-	const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
-
 }
